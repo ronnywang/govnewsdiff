b1e76f6c43393247677de285fcc26b572445bd1a
Ronny Wang <ronnywang@gmail.com>
2016-08-06 16:32:00 +0800

    加上行政院新聞

--- webdata/models/News.php
+++ webdata/models/News.php
@@ -166,23 +166,9 @@ class News extends Pix_Table
 
     public static function getSources()
     {
+        // 需要改 NewsRaw 同步加上 Crawler Class
         return array(
-            1 => '蘋果',
-            2 => '中時',
-            3 => '中央社',
-            4 => '東森',
-            5 => '自由',
-            6 => '新頭殼',
-            7 => 'NowNews',
-            8 => '聯合',
-            9 => 'TVBS',
-            10 => '中廣新聞網',
-            11 => '公視新聞網',
-            12 => '台視',
-            13 => '華視',
-            14 => '民視',
-//            15 => '三立',
-            16 => '風傳媒',
+            1 => '行政院本院新聞',
         );
     }
 }
--- webdata/models/NewsRaw.php
+++ webdata/models/NewsRaw.php
@@ -49,74 +49,9 @@ class NewsRaw extends Pix_Table
             return $ret;
         }
 
-        switch ($host) {
-        case 'www.chinatimes.com':
-            $ret = Crawler_Chinatimes::parse($raw, $url);
-            break;
-
-        case 'www.appledaily.com.tw':
-            $ret = Crawler_Appledaily::parse($raw, $url);
-            break;
-
-        case 'www.nownews.com':
-            $ret = Crawler_Nownews::parse($raw, $url);
-            break;
-
-        case 'www.ettoday.net':
-            $ret = Crawler_Ettoday::parse($raw, $url);
-            break;
-
-        case 'newtalk.tw':
-            $ret = Crawler_Newtalk::parse($raw, $url);
-            break;
-
-        case 'iservice.libertytimes.com.tw':
-        case 'news.ltn.com.tw':
-            $ret = Crawler_Libertytimes::parse($raw, $url);
-            break;
-
-        case 'www.libertytimes.com.tw':
-            $ret = Crawler_Libertytimes::parse2($raw, $url);
-            break;
-
-        case 'www.cna.com.tw':
-            $ret = Crawler_CNA::parse($raw, $url);
-            break;
-
-        case 'udn.com':
-            $ret = Crawler_UDN::parse($raw, $url);
-            break;
-
-        case 'news.tvbs.com.tw':
-            $ret = Crawler_TVBS::parse($raw, $url);
-            break;
-
-        case 'www.bcc.com.tw':
-            $ret = Crawler_BCC::parse($raw, $url);
-            break;
-
-        case 'news.pts.org.tw':
-            $ret = Crawler_PTS::parse($raw, $url);
-            break;
-
-        case 'www.ttv.com.tw':
-            $ret = Crawler_TTV::parse($raw, $url);
-            break;
-
-        case 'news.cts.com.tw':
-            $ret = Crawler_CTS::parse($raw, $url);
-            break;
-
-        case 'news.ftv.com.tw':
-            $ret = Crawler_FTV::parse($raw, $url);
-            break;
-
-        case 'www.setnews.net':
-            $ret = Crawler_SETNews::parse($raw, $url);
-            break;
-
-        case 'www.stormmediagroup.com':
-            $ret = Crawler_StormMediaGroup::parse($raw, $url);
+        switch ($news->source) {
+        case 1:
+            $ret = Crawler_EyNews::parse($raw, $url);
             break;
 
         default:
