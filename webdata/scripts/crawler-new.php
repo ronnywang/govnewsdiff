0d004fad3a630bbb64f8d692efe652dc9c2742bf
Ronny Wang <ronnywang@gmail.com>
2016-08-06 17:14:12 +0800

    改用 News::getSources() 爬所有來源

--- webdata/scripts/crawler-new.php
+++ webdata/scripts/crawler-new.php
@@ -3,34 +3,17 @@
 include(__DIR__ . '/../init.inc.php');
 Pix_Table::$_save_memory = true;
 
-$crawlers = array(
-    1 => 'Crawler_Appledaily',
-    2 => 'Crawler_Chinatimes',
-    3 => 'Crawler_CNA',
-    4 => 'Crawler_Ettoday',
-    5 => 'Crawler_Libertytimes',
-    6 => 'Crawler_Newtalk',
-    7 => 'Crawler_Nownews',
-    8 => 'Crawler_UDN',
-    9 => 'Crawler_TVBS',
-    10 => 'Crawler_BCC',
-    11 => 'Crawler_PTS',
-    12 => 'Crawler_TTV',
-    13 => 'Crawler_CTS',
-    14 => 'Crawler_FTV',
-//    15 => 'Crawler_SETNews',
-    16 => 'Crawler_StormMediaGroup'
-);
+$sources = News::getSources();
 
 $start = microtime(true);
 $insert_count = 0;
 $max_insert = 500;
-$keys = array_keys($crawlers);
+$keys = array_keys($sources);
 // 隨機跑新聞來源，避免在前面的總是優先被跑到
 shuffle($keys);
 
 foreach ($keys as $id) {
-    $class = $crawlers[$id];
+    $class = $sources[$id][1];
     if ($max_insert - $insert_count <= 0) {
         break;
     }
