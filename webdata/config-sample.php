9b0f4a146dc21025a6e023795180d3742d0fb895
Ronny Wang <ronnywang@gmail.com>
2016-08-12 00:24:00 +0800

    加上 config-sample.php  ，並預設打開 sqlite 設定

---
+++ webdata/config-sample.php
@@ -0,0 +1,5 @@
+<?php
+
+putenv('DATABASE_URL=sqlite://' . __DIR__ . '/db.sqlite');
+// putenv('DATABASE_URL=mysql://user:pass@123.123.123.123/db');
+// putenv('DATABASE_URL=pgsql://user:pass@123.123.123.123/db');
